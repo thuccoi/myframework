914b820459a66d52e6d26e9f68788576db7296a1
Nguyen The Thuc <thucfami@gmail.com>
2018-10-16 23:50:06 +0700

    package

--- system/Template/ControllerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace system\Template;
-
-interface ControllerInterface {
-
-    public function __invoke($requestName, \system\Router $router, \system\Helper\Code $code, array $config = null, array $options = null);
-}
